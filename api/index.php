294fb17d3bef05d5ce97564d1fd1b48ee6cfbae7
Zefa <zefanyamandagiti@gmail.com>
2026-07-17 13:56:56 +0800

    Fix Vercel read-only filesystem error

--- api/index.php
+++ api/index.php
@@ -1,4 +1,24 @@
 <?php
 
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
+if (!is_dir('/tmp/storage')) {
+    mkdir('/tmp/storage/logs', 0777, true);
+    mkdir('/tmp/storage/framework/cache', 0777, true);
+    mkdir('/tmp/storage/framework/views', 0777, true);
+    mkdir('/tmp/storage/framework/sessions', 0777, true);
+}
+
+$app->useBootstrapPath($_ENV['APP_BOOTSTRAP_CACHE'] ?? '/tmp/bootstrap/cache');
+if (!is_dir('/tmp/bootstrap/cache')) {
+    mkdir('/tmp/bootstrap/cache', 0777, true);
+}
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
